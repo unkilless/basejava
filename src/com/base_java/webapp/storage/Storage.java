2f2fcbc780e18360aa4cca2ac4c5c78f4204ee1a
unkilless <lastbnol@yandex.ru>
2020-02-29 17:11:46 +0300

    HW-3. Собираем проект из patch, добавил Storage

---
+++ src/com/base_java/webapp/storage/Storage.java
@@ -0,0 +1,22 @@
+package com.base_java.webapp.storage;
+
+import com.base_java.webapp.model.Resume;
+
+public interface Storage {
+
+    int sizeOfArray();
+
+    void update(Resume resumeForUpd);
+
+    void save(Resume savingResume);
+
+    Resume[] getAll();
+
+    Resume getByID(Integer ID);
+
+    void deleteByID(Integer ID);
+
+    void deleteAll();
+
+    int[] getIdByValue(String findingStr);
+}
